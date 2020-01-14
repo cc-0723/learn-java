be8538bc6a8168cd6ad6be92ad99808f3ec0da03
常铖 <1326881231@qq.com>
2020-01-14 19:12:23 +0800

    example

---
+++ Solution13.java
@@ -0,0 +1,13 @@
+package 寒假作业;
+
+public class Solution13 {
+    //合并两个有序数组
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int len = m + n - 1;
+        while(len1 >= 0 && len2 >= 0)
+            nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+        System.arraycopy(nums2,0,nums1,0,len2 + 1);
+    }
+}
