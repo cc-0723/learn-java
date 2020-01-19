050e569c7ad1c87df062eae3d3c28048921d32c3
常铖 <1326881231@qq.com>
2020-01-19 18:58:01 +0800

    example

---
+++ Solution16.java
@@ -0,0 +1,18 @@
+package 寒假作业;
+
+public class Solution16 {
+    //长建按入
+    public boolean isLongPressedName(String name, String typed) {
+        char [] c1 = name.toCharArray();
+        char [] c2 = typed.toCharArray();
+        int p1 = 0, p2 = 0;
+
+        while(p1 < c1.length && p2 < c2.length) {
+            if(c1[p1] == c2[p2]) {
+                p1++;
+            }
+            p2++;
+        }
+        return p1 == c1.length;
+    }
+}
