3eaed8f30bdf376f65a2456250570a266e42baaa
常铖 <1326881231@qq.com>
2020-02-05 20:26:45 +0800

    example

---
+++ Solution24.java
@@ -0,0 +1,14 @@
+package 寒假作业;
+
+public class Solution24 {
+    //两数之和
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] == target - nums[i])
+                    return new int[] {i,j};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
