60ef952fef5beec8b849050668c4e0fb3b43410a
常铖 <1326881231@qq.com>
2020-02-23 13:34:26 +0800

    example

---
+++ Solution38.java
@@ -0,0 +1,31 @@
+package algorithms.com.guan.javajicu;
+public class TestDemo
+{
+    public static String output = ””;
+    public static void foo(inti)
+    {
+        try
+        {
+            if (i == 1)
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            output += “2”;
+            return ;
+        } finally
+        {
+            output += “3”;
+        }
+        output += “4”;
+    }
+    public static void main(String[] args)
+    {
+        foo(0);
+        foo(1);
+        System.out.println(output);
+    }
+}
+//3423
