53a9eb16880b053e5d7e6bb8492026c18977644c
常铖 <1326881231@qq.com>
2020-02-25 12:53:53 +0800

    example

---
+++ Solution40.java
@@ -0,0 +1,19 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        int y = 0;
+        int k = 0;
+        for (int z = 0; z < 5; z++) {
+            if ((++x > 2) && (++y > 2) && (k++ > 2))
+            { 
+                x++;
+                ++y;
+                k++;
+            }
+        }
+        System.out.println(x + ”” +y + ”” +k);
+    }
+}
+//5 3 1 
