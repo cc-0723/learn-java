323961ff4c843cc66c59b9422244b2f507316b6f
常铖 <1326881231@qq.com>
2020-03-03 13:30:48 +0800

    example

---
+++ Solution_3.java
@@ -0,0 +1,28 @@
+package 寒假练习;
+
+public class Solution_3 {
+    class Value{
+        public int i=15;
+    }
+        public static void main(String argv[]){
+            Solution_3 t = new Solution_3();
+            t.first();
+        }
+
+        public void first(){
+            int i=5;
+            Value v=new Value( );
+            v.i=25;
+            second(v,i);
+            System.out.println(v.i);
+        }
+
+        public void second(Value v,int i){
+            i = 0;
+            v.i = 20;
+            Value val = new Value( );
+            v = val;
+            System.out.println(v.i+" "+i);// 15 0
+        }
+    }
+//15 0 20
