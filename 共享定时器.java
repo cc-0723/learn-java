f28645179e0ace637ac0dbebeba04f515558e1fd
常铖 <1326881231@qq.com>
2020-04-19 13:57:52 +0800

    example

---
+++ 共享定时器.java
@@ -0,0 +1,23 @@
+package 多线程;
+
+public class 共享定时器 {
+    private static MyTimer timer = new MyTimer();
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            timer.schedule(new MyTimer.MyTimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("随便什么");
+                }
+            }, 10 * 1000);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new MyThread().start();
+        }
+    }
+}
