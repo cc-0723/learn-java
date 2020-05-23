5c383e7e87dbac951f00b259c903198f221e6b6e
常铖 <1326881231@qq.com>
2020-05-23 19:24:03 +0800

    example

---
+++ NoServiceState.java
@@ -0,0 +1,33 @@
+package 设计模式实验;
+
+public class NoServiceState implements ATMState{
+    private ATM atm;//保留状态持有者的引用，以便对其进行操作
+    public NoServiceState(ATM atm){
+        this.atm = atm;
+    }
+
+    @Override
+    public void insertCard() {
+        System.out.println("插卡失败，机器发生了故障");
+    }
+
+    @Override
+    public void submitPwd() {
+        System.out.println("密码提交失败，机器发生了故障");
+    }
+
+    @Override
+    public void getCash() {
+        System.out.println("取款失败，机器发生了故障");
+    }
+
+    @Override
+    public void queryBalance() {
+        System.out.println("余额查询失败，机器发生了故障");
+    }
+
+    @Override
+    public void ejectCard() {
+        System.out.println("取卡失败，机器发生了故障");
+    }
+}
