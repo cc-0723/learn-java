e2c28c03a199560a22fa1043eb053d716c4d9ebd
常铖 <1326881231@qq.com>
2020-05-25 11:36:58 +0800

    example

---
+++ GobangTest.java
@@ -0,0 +1,11 @@
+package 人工智能实验;
+
+//简单测试类
+public class GobangTest {
+    public static void main(String args[]) {
+        @SuppressWarnings("unused")
+        GobangFrame frame = new GobangFrame();
+    }
+
+
+}
